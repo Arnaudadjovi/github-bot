5be54563aa3c4d4dbfa72c9668ed45cb122808a3
Arnaud <arnaud2adjovi276@gmail.com>
2022-08-26 11:30:13 +0100

    test

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
     $output = '';
 
-        for ($i=1; $i < 10; $i++) { 
-            $day = $i;
+        for ($i=1; $i < 500; $i++) { 
+            $day = 10 + $i;
             $commit = 'git commit --date="' . $day . ' day ago" -m "test"';
     
             $output = shell_exec('echo test >> README.md');
